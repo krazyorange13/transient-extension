3232819ba9f898249d4048963bd5997becbb6f40
krazy orange <krazyorange13@gmail.com>
2024-12-01 18:32:35 -0600

    smol lil' change

--- src/graphics.c
+++ src/graphics.c
@@ -39,13 +39,13 @@ void window_draw_line
     int dx = abs(x2 - x1);
     int dy = abs(y2 - y1);
     int sx = x1 < x2 ? 1 : -1;
-    int sy = y1 < y2 ? 1 : -1; 
+    int sy = y1 < y2 ? 1 : -1;
     int err = (dx > dy ? dx : -dy) / 2;
     int e2;
 
     for(;;)
     {
-        if (x1 >= 0 && y1 >= 0) window_set_pixel(win, x1, y1, c);
+        window_set_pixel(win, x1, y1, c);
         if (x1==x2 && y1==y2) break;
         e2 = err;
         if (e2 >-dx) { err -= dy; x1 += sx; }
